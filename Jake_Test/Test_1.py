056fb3892e61c08a5d6f500d326689d94e965ea3
Merlinja <Merlinja@gmail.com>
2013-10-16 15:32:28 -0700

    Uploading more old code.

--- Jake_Test/Test_1.py
+++ Jake_Test/Test_1.py
@@ -10,69 +10,13 @@ from ctypes import c_void_p
 
 #from shader import getShader
 #from transforms import *
-#from parametricsurface import ParametricSurface
+from parametricsurface import ParametricSurface
 #from pntbuffer import PNTBuffer
 #from uniforms import Uniforms
 #from drawable import Mesh
-#from frame import Frame
+from frame import Frame
 
-class ParametricSurface():
-    def __init__(self, point, norm = None, tex = None,
-                 sRange = None, tRange = None):
-        self.point = point
-        if norm != None:
-            self.norm = norm
-        if tex != None:
-            self.tex = tex
-        if sRange != None:
-            self.sRange = sRange
-        else:
-            self.sRange = numpy.arange(0.0, 1.1, 0.1)
-        if tRange != None:
-            self.tRange = tRange
-        else:
-            self.tRange = numpy.arange(0.0, 1.1, 0.1)
-        self.createBuffer()
-        
-    def norm(self, s, t):
-        a = numpy.array(self.point(s, t), dtype=numpy.float32)[0:3]
-        b = numpy.array(self.point(s+0.001, t), dtype=numpy.float32)[0:3]
-        c = numpy.array(self.point(s, t+0.001), dtype=numpy.float32)[0:3]
-        v1 = c-a
-        v2 = b-a
-        n = numpy.cross(v1, v2)
-        denom = numpy.sqrt(numpy.dot(n,n))
-        if denom > 0.0:
-            n /= numpy.sqrt(numpy.dot(n,n))
-        n = list(n)
-        n.append(0.0)
-        return n
 
-    def tex(self, s, t):
-        return [s,t]
-
-    def createBuffer(self):
-        # TODO: convert these to strips (natural for param surfaces)
-        vertices = []
-        for s in self.sRange:
-            for t in self.tRange:
-                p00 = self.point(s,t)
-                n00 = self.norm(s,t)
-                t00 = self.tex(s,t)
-                vertices.extend(p00+n00+t00)
-        self.vertices = numpy.array(vertices, dtype=numpy.float32)
-
-        numS = len(self.sRange)
-        numT = len(self.tRange)
-        indices = []
-        for s in range(numS-1):
-            for t in range(numT-1):
-                i00 = s*numT + t
-                i01 = s*numT + (t+1)
-                i10 = (s+1)*numT + t
-                i11 = (s+1)*numT + (t+1)
-                indices.extend([i00,i01,i10, i01,i11,i10])
-        self.indices = numpy.array(indices, dtype=numpy.uint16)
 
 radius = 1.0
 def spherePoint(s,t):
@@ -94,12 +38,110 @@ inc = numpy.pi*0.125
 sRange = numpy.arange(0.0, 2*numpy.pi+inc, inc)
 tRange = numpy.arange(0.0, numpy.pi+inc, inc)
 
+def init():
+    # Normal OpenGL initializations
+    glClearColor(0.5,0.5,0.5,1.0)
+    glEnable(GL_DEPTH_TEST)
+
+def display():
+    # OpenGL necessities first:
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((800,800), OPENGL|DOUBLEBUF)
+    frame = Frame()
+    rotX, rotY, rotZ = 0.0,0.0,0.0
+    rotXmatrix = numpy.eye(4, dtype=numpy.float32)
+    rotYmatrix = numpy.eye(4, dtype=numpy.float32)
+    rotZmatrix = numpy.eye(4, dtype=numpy.float32)
+    clock = pygame.time.Clock()
 
+    init()
+    near = 1.0 
+    far = 50.0 
+    depth = 2.0
 
-    theSurface = ParametricSurface(point = spherePoint, norm = sphereNorm,
-                                   sRange = sRange, tRange = tRange)
+    while True:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+            if event.type == KEYUP and event.key == K_ESCAPE:
+                return
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    n = theUniforms.items['showLines']
+                    theUniforms.items['showLines'] = (n+1)%2
+                if event.key == K_l:
+                    if near < 2.0:
+                        near = 19.0
+                        far = 21.0
+                    else:
+                        near = 1.0
+                        far = 50.0
+
+        # Keyboard polling:
+        pressed = pygame.key.get_pressed()
+
+        # observer movement:
+        amt = 0.2
+        if pressed[K_DOWN]:
+            frame.elevate(amt)
+        if pressed[K_UP]:
+            frame.elevate(-amt)
+        if pressed[K_LEFT]:
+            frame.strafe(-amt)
+        if pressed[K_RIGHT]:
+            frame.strafe(amt)
+        if pressed[K_PAGEUP]:
+            frame.move(amt)
+        if pressed[K_PAGEDOWN]:
+            frame.move(-amt)
+        
+        # world rotation:
+        if pressed[K_w]:
+            rotX -= 0.02
+        if pressed[K_s]:
+            rotX += 0.02
+        if pressed[K_a]:
+            rotY += 0.02
+        if pressed[K_d]:
+            rotY -= 0.02
+        if pressed[K_q]:
+            rotZ -= 0.02
+        if pressed[K_e]:
+            rotZ += 0.02
+
+        #setRotationX(rotXmatrix, rotX)
+        #setRotationY(rotYmatrix, rotY)
+        #setRotationZ(rotZmatrix, rotZ)
+
+        mat = numpy.dot(rotYmatrix, rotZmatrix)
+        mat = numpy.dot(rotXmatrix, mat)
+
+        # near/far manipulation:
+        if pressed[K_j]:
+            near += amt
+        if pressed[K_k]:
+            near -= amt
+        if pressed[K_u]:
+            far += amt
+        if pressed[K_i]:
+            far -= amt
+
+        if pressed[K_o]:
+            near += amt
+            far += amt
+        if pressed[K_p]:
+            near -= amt
+            far -= amt
+
+                            
+        display()
+        pygame.display.flip()
+
+    
 
 if __name__ == '__main__':
     try:
